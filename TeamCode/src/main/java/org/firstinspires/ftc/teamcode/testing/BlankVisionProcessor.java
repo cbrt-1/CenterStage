67d7d4c7fee93a5a620cf71d172846bcc4bd6138
cbrt-1 <93866366+cbrt-1@users.noreply.github.com>
2024-01-21 16:16:01 -0600

    Add blank vision processor
    Might plan on rewriting the OpenCV pipeline to instead extend from vision processor in order to take advantage of the GPU accelerated thread.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/BlankVisionProcessor.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class BlankVisionProcessor implements VisionProcessor {
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        // Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2GRAY);
+        return null;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
+
+}
