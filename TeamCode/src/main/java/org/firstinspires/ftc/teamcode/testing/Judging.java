48c26ec842f8d22c1859a69736c96adc3e0961eb
cbrt-1 <93866366+cbrt-1@users.noreply.github.com>
2024-01-23 22:26:40 -0600

    Add Judging.java
    Added a blank opmode with a todo list.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Judging.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+@Disabled
+public class Judging extends OpMode {
+
+    /* 
+        TODO:
+        Controls:
+        - Extend and retract odo pods
+        - PIDF lift
+        - Claw State Machine
+        * Automatic Pickup (?)
+    */
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+
+}
